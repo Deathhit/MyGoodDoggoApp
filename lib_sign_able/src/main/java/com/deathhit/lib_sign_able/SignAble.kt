740b10b399769b6ea129fec3b52ed495a795f107
20001078 <mark_yang@books.com.tw>
2022-03-15 15:17:13 +0800

    Implement SignAbleTest.

--- lib_sign_able/src/main/java/com/deathhit/lib_sign_able/SignAble.kt
+++ lib_sign_able/src/main/java/com/deathhit/lib_sign_able/SignAble.kt
@@ -2,7 +2,7 @@ package com.deathhit.lib_sign_able
 
 import java.util.*
 
-class SignAble<Content>(val content: Content? = null) {
+class SignAble<Content>(private val content: Content? = null) {
     private val signatureMap: WeakHashMap<Any, Boolean> by lazy { WeakHashMap() }
 
     fun sign(any: Any, onSigned: (content: Content) -> Unit) {
