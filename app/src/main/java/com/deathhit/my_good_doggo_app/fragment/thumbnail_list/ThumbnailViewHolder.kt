15cb2a94a7d0cf9ce9c2a626fc6941e96d7fcf33
20001078 <mark_yang@books.com.tw>
2022-05-09 18:29:46 +0800

    Minor adjustments.

--- app/src/main/java/com/deathhit/my_good_doggo_app/fragment/thumbnail_list/ThumbnailViewHolder.kt
+++ app/src/main/java/com/deathhit/my_good_doggo_app/fragment/thumbnail_list/ThumbnailViewHolder.kt
@@ -4,7 +4,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.deathhit.my_good_doggo_app.databinding.ItemThumbnailListThumbnailBinding
 import com.deathhit.my_good_doggo_app.model.ThumbnailVO
 
-class ThumbnailViewHolder(val binding: ItemThumbnailListThumbnailBinding) :
-    RecyclerView.ViewHolder(binding.root) {
+class ThumbnailViewHolder(
+    val binding: ItemThumbnailListThumbnailBinding,
     var item: ThumbnailVO? = null
-}
+) : RecyclerView.ViewHolder(binding.root)
